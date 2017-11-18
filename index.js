29ce5abfc3eebcdb2e54697b291c802843c7a9e3
Michal Koudelka <koudelka.michal@gmail.com>
2017-11-18 18:11:22 +0100

    Added empty client reference

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const db = require('./lib/data');
 
 const app = express();
-app.use(express.static('public'));
+app.use('/', express.static('client'));
 
 var opened = false;
 
